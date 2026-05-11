66d0c54dfaad16564e98b335a55390c43ad89eb8
jahidhasan-webdev01 <jahidhasan.webdev01@gmail.com>
2026-05-11 10:33:15 +0600

    add custome logo for both theme

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,27 @@
 import Link from "next/link";
+import logoDark from "@/assests/logo-dark.png";
+import logoLight from "@/assests/logo-light.png";
+import Image from "next/image";
 
 const Logo = () => {
     return (
         <>
             <Link href={"/"}>
-                Jahid Hasan
+                <Image
+                    src={logoLight}
+                    alt="logo"
+                    height={300}
+                    width={300}
+                    className="block dark:hidden w-20 h-auto"
+                />
+
+                <Image
+                    src={logoDark}
+                    alt="logo"
+                    height={300}
+                    width={300}
+                    className="hidden dark:block w-20 h-auto"
+                />
             </Link>
         </>
     );
